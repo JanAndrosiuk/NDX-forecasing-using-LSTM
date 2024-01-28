29.01.2024 Transaction cost, visualization - Corrected transaction cost calculation - Added equity line visualization for multiple test cases - Logging is now a bit more clean - Added bash script to run code in a loop

src/data/windowSplit.py
@@ -14,6 +14,7 @@ class WindowSplit:
         self.config = self.setup.config
         self.logger = logging.getLogger("Window Split")
         self.logger.addHandler(logging.StreamHandler())
+        self.logger.info("[[Window split module]]")
         self.params = {
             "target": self.config["model"]["VarTarget"],
             "features": self.config["model"]["Features"].split(', '),
@@ -103,10 +104,8 @@ class WindowSplit:
             'scalers': np.asarray(scalers)
         }
 
-        self.logger.info(f"Train window dimensions (features, targets): {windows_dict['x_train'].shape}, "
-                         f"{windows_dict['y_train'].shape}")
-        self.logger.info(f"Test window dimensions (features, targets): {windows_dict['x_test'].shape}, "
-                         f"{windows_dict['y_test'].shape}")
+        self.logger.info(f"\nTrain window dimensions (features, targets): {windows_dict['x_train'].shape}, "f"{windows_dict['y_train'].shape}")
+        self.logger.info(f"Test window dimensions (features, targets): {windows_dict['x_test'].shape}, "f"{windows_dict['y_test'].shape}\n")
 
         # Pickle the result dictionary
         with open(self.config["prep"]["WindowSplitDict"], 'wb') as handle:

src/model/modelFitPredict.py
@@ -37,18 +37,22 @@ class RollingLSTM:
         if not os.path.isdir(self.export_path): os.mkdir(self.export_path)
         self.logger = logging.getLogger("Fit Predict") # type: ignore
         self.logger.addHandler(logging.StreamHandler()) # type: ignore
+        self.logger.info("[[Model Fit & Predict module]]\n")
         print = self.logger.info
         self.tensorboard_logger = self.config["logger"]["TensorboardLoggerPath"]
         
         self.icsa_df_raw_path = self.setup.ROOT_PATH + self.config["raw"]["IcsaRawDF"]
         with open(self.config["prep"]["WindowSplitDict"], 'rb') as handle: self.window_dict = pickle.load(handle)
-        self.logger.info(f'Successfully loaded split dictionary\n'\
+        self.logger.info(f'\nSuccessfully loaded split dictionary\n'\
                          f'GPU DETECTED: [{tf.test.is_gpu_available(cuda_only=True)}]\n'\
                          f'TRAIN (features; targets): ({self.window_dict["x_train"].shape}; {self.window_dict["y_train"].shape})\n'\
-                         f'TEST: ({self.window_dict["x_test"].shape}; {self.window_dict["y_test"].shape})')
+                         f'TEST: ({self.window_dict["x_test"].shape}; {self.window_dict["y_test"].shape})\n')
         self.x_train, self.y_train, self.x_test = (self.window_dict['x_train'], self.window_dict['y_train'], self.window_dict['x_test'])
         
+        self.logger.info(f"Parameters summary:\n{'-'*49}")
         for key in self.config["model"]: self.logger.info(f"{key}: {self.config['model'][key]}")
+        self.logger.info(f"{'-'*49}\n")
+            
         self.early_stopping_min_delta = 0.0
         self.predictions = []
 
@@ -76,7 +80,7 @@ class RollingLSTM:
 
         if i == 0:
             validation_set_shapes = (self.x_train[i][-validation_window_size:].shape, self.y_train[i][-validation_window_size:].shape)
-            print(f"TRAIN (features, targets): ({self.x_train.shape}, {self.y_train.shape})\nVAL: ({validation_set_shapes[0]}, {validation_set_shapes[1]})")
+            print(f"Validation set size: ({validation_set_shapes[0]}, {validation_set_shapes[1]})\n\nSTARTING TRAINING PROCESS\n")
         
         # Validation & Early Stopping setup
         es = EarlyStopping(monitor='val_loss', mode='min', verbose=0, patience=3, restore_best_weights=True, min_delta=self.early_stopping_min_delta)
@@ -282,6 +286,7 @@ class RollingLSTM:
         df_pred_eval.to_csv(f'{self.config["prep"]["DataOutputDir"]}model_eval_data_{self.timestamp}.csv', index=False)
         df_pred_eval.set_index("Date", inplace=True)
         df_pred_eval.to_pickle(f'{self.config["prep"]["DataOutputDir"]}model_eval_data_{self.timestamp}.pkl')
+        self.logger.info("Success\n")
         
         return 0
     

src/model/performanceMetrics.py
@@ -3,10 +3,10 @@ import sys
 import os
 import pickle
 import shutil
-import numpy as np
-from collections import Counter
 import json
 import time
+from collections import Counter
+import numpy as np
 
 
 class PerformanceMetrics:
@@ -15,12 +15,13 @@ class PerformanceMetrics:
         self.config = self.setup.config
         self.logger = logging.getLogger("Performance Metrics")
         self.logger.addHandler(logging.StreamHandler())
+        self.logger.info("[[Performance Metrics module]]")
         self.eval_data = None
         self.eval_data_timestamp = None
         self.pred_thres = float(self.config["model"]["PredictionThreshold"])
         self.tr_cost = float(self.config["evaluation"]["TransactionCost"])
         self.export_path = ""
-
+        
     def calculate_metrics(self, custom_timestamp=None):
         """
         Calculate ARC (for buy-and-hold strategy and LSTM strategy), ASD, IR, MLD (only for LSTM strategy)
@@ -29,19 +30,31 @@ class PerformanceMetrics:
 
         # Load evaluation data for performance metrics
         self.load_eval_data(custom_timestamp)
+        predictions_array = self.eval_data["Pred"].values
+        
+        # Position-related metrics
+        pos_counter = Counter(np.sign(predictions_array))
+        position_change_counter = sum([1 if np.sign(predictions_array[i-1]) != np.sign(predictions_array[i]) else 0 for i in range(1, predictions_array.shape[0])])
+        self.logger.info(f'Positions: [Long]: {pos_counter[1]}, [Short]: {pos_counter[-1]}, [0]: {pos_counter[0]}, Position changes: {position_change_counter}\n')
+        
+        # Create Equity Line
         equity_line_array, returns_array = (
-            self.equity_line(self.eval_data["Pred"].values, self.eval_data["Real"].values) 
+            self.equity_line(predictions_array, self.eval_data["Real"].values) 
         )
-        pos_counter = Counter(np.sign(self.eval_data["Pred"].values))
-        self.logger.info(f'[Positions] Long: {pos_counter[1]}, Short: {pos_counter[-1]}, 0: {pos_counter[0]}')
-
+        
         # Save performance statistics to a dictionary
         metrics = {
-            "[ARC_BH]": str(np.round(self.arc(self.eval_data["Real"].values) * 100, 2)) + "%",
-            "[ARC_EQ]": str(np.round(self.arc(equity_line_array) * 100, 2)) + "%",
-            "[ASD_EQ]": str(np.round(self.asd(equity_line_array), 4)),
-            "[IR_EQ]": str(np.round(self.ir(equity_line_array), 4)),
-            "[MLD_EQ]": str(np.round(self.mld(returns_array) * 100, 2)) + "% of the year"
+            "BH_ARC": str(format(np.round(self.arc(self.eval_data["Real"].values) * 100, 2), '.2f')),
+            "BH_ASD": str(format(np.round(self.asd(self.eval_data["Real"].values), 4), '.4f')),
+            "BH_MLD": str(format(np.round(self.mld_bh(self.eval_data["Real"].values) * 100, 2), '.2f')),
+            "BH_IR": str(format(np.round(self.ir(self.eval_data["Real"].values), 4), '.4f')),
+            "BH_IR**": str(format(np.round(self.ir2(self.eval_data["Real"].values, returns_array, use_equity_line_for_mld=True), 4), '.4f')),
+            "EQ_ARC": str(format(np.round(self.arc(equity_line_array) * 100, 2), '.2f')),
+            "EQ_ASD": str(format(np.round(self.asd(equity_line_array), 4), '.4f')),
+            "EQ_MLD": str(format(np.round(self.mld(returns_array) * 100, 2), '.2f')),
+            "EQ_IR": str(format(np.round(self.ir(equity_line_array), 4), '.4f')),
+            "EQ_IR**": str(format(np.round(self.ir2(equity_line_array, returns_array), 4), '.4f')),
+            "POS_CNT": str(position_change_counter)
         }
 
         # Save results to .json file
@@ -50,16 +63,10 @@ class PerformanceMetrics:
             json.dump(metrics, fp, indent=4, sort_keys=False)
         shutil.copy2(performance_metrics_path, self.export_path)
         eq_line_path = f'{self.setup.ROOT_PATH}{self.config["prep"]["DataOutputDir"]}eq_line_{self.eval_data_timestamp}.pkl'
-        self.logger.info(f'Saving Equity Line array: {eq_line_path}')
+        self.logger.info(f'Saved Equity Line array:\t\t{eq_line_path}\n')
         with open(eq_line_path, 'wb') as handle:
             pickle.dump(np.asarray(equity_line_array), handle, protocol=pickle.HIGHEST_PROTOCOL)
         shutil.copy2(eq_line_path, self.export_path)
-
-        self.logger.info(f'[ARC_BH]:    {metrics["[ARC_BH]"]}')
-        self.logger.info(f'[ARC_EQ]:    {metrics["[ARC_EQ]"]}')
-        self.logger.info(f'[ASD_EQ]:    {metrics["[ASD_EQ]"]}')
-        self.logger.info(f'[IR_EQ]:     {metrics["[IR_EQ]"]}')
-        self.logger.info(f'[MLD_EQ]:    {metrics["[MLD_EQ]"]}')
 
         return equity_line_array
 
@@ -72,8 +79,9 @@ class PerformanceMetrics:
             except ValueError as ve:
                 self.logger.error("No file available. Please rerun the whole process / load data first.")
                 sys.exit(1)
-            self.logger.info(f"Found latest eval data pickle: {latest_file}")
+            self.logger.info(f"Latest found eval-data pickle:\t\t{latest_file}")
             self.eval_data_timestamp = latest_file[-20:-4]
+            with open(latest_file, 'rb') as handle: self.eval_data = pickle.load(handle)
         else: 
             self.eval_data_timestamp = custom_timestamp
             custom_eval_data_path = f'{self.config["prep"]["DataOutputDir"]}model_eval_data_{custom_timestamp}.pkl'
@@ -83,7 +91,8 @@ class PerformanceMetrics:
             except FileNotFoundError as ve: 
                 self.logger.error("Model evaluation data not found.")
                 sys.exit(1)
-            self.logger.info(f"Loaded model evaluation data pickle: {custom_eval_data_path}")
+        
+        self.logger.info(f"Loaded\n")
 
         self.export_path = f'{self.setup.ROOT_PATH}{self.config["prep"]["ExportDir"]}{self.eval_data_timestamp}/'
         if not os.path.isdir(self.export_path): os.mkdir(self.export_path)
@@ -108,6 +117,11 @@ class PerformanceMetrics:
         | S        | L       | abs(x)<thres | S (keep)   |
         | S        | S       | abs(x)>thres | S (keep)   |
         | S        | S       | abs(x)<thres | S (keep)   |
+        
+        transaction costs due to position change:
+        0-0, L-L, S-S => 0
+        0<->L, 0<->S  => 1
+        S<->L         => 2
 
         :param predictions: array of values between [-1, 1]
         :param actual_values: array of actual values
@@ -117,30 +131,36 @@ class PerformanceMetrics:
         
         eq_line_array = [actual_values[0]]
         eq_line_second_return = (actual_values[1] - actual_values[0]) / actual_values[0]
-        if (predictions[0] > 0): eq_line_array.append(eq_line_array[0] * (1 + eq_line_second_return))
-        elif (predictions[0] < 0): eq_line_array.append(eq_line_array[0] * (1 - eq_line_second_return))
-        else: eq_line_array.append(eq_line_array[0])
+        if (predictions[0] > 0): 
+            eq_line_array.append(eq_line_array[0] * (1 + eq_line_second_return))
+        elif (predictions[0] < 0): 
+            eq_line_array.append(eq_line_array[0] * (1 - eq_line_second_return))
+        else: 
+            eq_line_array.append(eq_line_array[0])
         
         self.logger.debug(f'Real values (B&H) -> First 5: {actual_values[:5]}, Last 5: {actual_values[-5:]}')
         
         for i in range(1, actual_values.shape[0]-1, 1):
-            # r = (actual_values[i] - actual_values[i-1])/actual_values[i-1]
-            # print(self.eval_data.iloc[[i]])
-            # print(f'{i} {predictions[i-1]} {predictions[i]} {r} {eq_line_array[i-1]}')
             
-            if (predictions[i] == 0): _return_rate = 0 - int(predictions[i-1] == 0) * self.tr_cost
+            # L->0, S->0, 0->0
+            if (predictions[i] == 0): 
+                _return_rate = 0 - int(predictions[i-1] != 0) * self.tr_cost
 
-            # L/L
-            elif (predictions[i-1] >= 0 and predictions[i] > 0): _return_rate = (actual_values[i+1] - actual_values[i]) / actual_values[i] - int(predictions[i] == 0) * self.tr_cost
+            # L->L, 0->L
+            elif (predictions[i-1] >= 0 and predictions[i] > 0):
+                _return_rate = (actual_values[i+1] - actual_values[i]) / actual_values[i] - int(predictions[i-1] == 0) * self.tr_cost
             
-            # L/S
-            elif (predictions[i-1] >= 0 and predictions[i] < 0): _return_rate = (actual_values[i] - actual_values[i+1]) / actual_values[i] - 2 * self.tr_cost + int(predictions[i] == 0) * self.tr_cost
+            # S->S, 0->S
+            elif (predictions[i-1] <= 0 and predictions[i] < 0):
+                _return_rate = (actual_values[i] - actual_values[i+1]) / actual_values[i] - int(predictions[i-1] == 0) * self.tr_cost
                 
-            # S/S
-            elif (predictions[i-1] <= 0 and predictions[i] < 0): _return_rate = (actual_values[i] - actual_values[i+1]) / actual_values[i] - int(predictions[i] == 0) * self.tr_cost
-
-            # S/L
-            elif (predictions[i-1] <= 0 and predictions[i] > 0): _return_rate = (actual_values[i+1] - actual_values[i]) / actual_values[i] - 2 * self.tr_cost + int(predictions[i] == 0) * self.tr_cost
+            # L->S
+            elif (predictions[i-1] > 0 and predictions[i] < 0):
+                _return_rate = (actual_values[i] - actual_values[i+1]) / actual_values[i] - 2 * self.tr_cost
+                
+            # S->L
+            elif (predictions[i-1] < 0 and predictions[i] > 0):
+                _return_rate = (actual_values[i+1] - actual_values[i]) / actual_values[i] - 2 * self.tr_cost
 
             returns_array.append(_return_rate)
             eq_line_array.append(eq_line_array[i] * (1 + _return_rate))
@@ -190,39 +210,59 @@ class PerformanceMetrics:
         """
         Information Ratio
         :param equity_array: Equity Line array
-        :param equity_array: array of investment return for each timestep
         :param scale: number of days required for normalization. By default in a year there are 252 trading days.
         """
         return self.arc(equity_array, scale) / self.asd(equity_array, scale)
+    
+    def ir2(self, equity_array: np.array, returns_array: np.array, scale=252, use_equity_line_for_mld=False) -> float:
+        """ Information Ratio ** -> combination of the IR, aRC, and MD metrics.
+        :param equity_array: Equity Line array
+        :param returns_array: array of investment return for each timestep
+        :param scale: number of days required for normalization. By default in a year there are 252 trading days.
+        """
+        ir = self.ir(equity_array, scale)
+        arc = self.arc(equity_array, scale)
+        if use_equity_line_for_mld: mld = self.mld_bh(equity_array, scale)
+        else: mld = self.mld(returns_array, scale)
+        
+        return  ir * arc * np.sign(arc) / mld
 
     @staticmethod
     def mld(returns_array: np.array, scale: int = 252) -> float:
-        """
-        Maximum Loss Duration
-        Maximum number of time steps when the returns were below 0
+        """Maximum Loss Duration -> max number of time steps when returns < 0
         :param returns_array: array of investment returns
         :param scale: number of days required for normalization. By default, in a year there are 252 trading days.
         :return: MLD
         """
-        max_loss = 0
-        curr = 0
-        for i in range(returns_array.shape[0]):
+        max_loss_duration = 0
+        current_loss_duration = 0
+        
+        if returns_array[0] < 0:
+            max_loss_duration = current_loss_duration
 
-            # If first returns is negative, add this occurrence to max loss counter
-            # If it's positive, continue
-            if i == 0 and returns_array[0] < 0:
-                curr += 1
-                max_loss = curr
+        for i in range(1, returns_array.shape[0]):
+            if (returns_array[i] < 0):
+                current_loss_duration += 1
+                if current_loss_duration > max_loss_duration:
+                    max_loss_duration = current_loss_duration
+            else: current_loss_duration = 0
 
-            # If the equity continues dropping
-            elif (i > 0) and (returns_array[i-1] < 0) and (returns_array[i] < 0):
-                curr += 1
-                if max_loss < curr:
-                    max_loss = curr
+        return max_loss_duration / scale
 
-            # If the equity stops dropping
-            elif (i > 0) and (returns_array[i-1] < 0) and (returns_array[i] > 0):
-                curr = 0
-
-        # Normalize over the number of trading days in a year
-        return max_loss / scale
+    @staticmethod
+    def mld_bh(equity_line_benchmark: np.array, scale: int = 252) -> float:
+        """Maximum Loss Duration based on equity line
+        :param returns_array: Equity Line array
+        :param scale: annual scaler
+        """
+        max_loss_duration = 0
+        current_loss_duration = 0
+        
+        for i in range(1, equity_line_benchmark.shape[0]):
+            if equity_line_benchmark[i-1] > equity_line_benchmark[i]:
+                current_loss_duration += 1
+                if current_loss_duration > max_loss_duration:
+                    max_loss_duration = current_loss_duration
+            else: current_loss_duration = 0
+            
+        return max_loss_duration / scale

src/visualization/plotResults.py
@@ -2,19 +2,22 @@ from src.init_setup import *
 import sys
 import os
 import pickle
+import datetime
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from dateutil.relativedelta import relativedelta
 import json
 import shutil
 
-
 class Plots:
-    def __init__(self) -> None:
+    def __init__(self, program_start_time: datetime) -> None:
         self.setup = Setup()
         self.config = self.setup.config
         self.logger = logging.getLogger("Generate Visualizations")
         self.logger.addHandler(logging.StreamHandler())
+        self.logger.info("[[Visualizations module]]")  
         self.eval_data = None
         self.timestamp = None
         self.model_config_dict, self.perf_metr_dict = None, None
@@ -22,8 +25,11 @@ class Plots:
         self.window_dict = None
         self.vis_dir = self.config["prep"]["VisualizationsDir"]
         if not os.path.isdir(self.vis_dir): os.makedirs(self.vis_dir)
-        self.csfont = {'fontname': 'Times New Roman'}
-        self.export_path = ""
+        self.export_path = "" 
+        plt.rcParams["font.family"] = "monospace"
+        plt.rcParams['axes.titlepad'] = 20
+        self.program_start_time = program_start_time
+        
 
     def load_performance_data(self, custom_timestamp=None) -> int:
         if custom_timestamp is None:
@@ -36,7 +42,7 @@ class Plots:
                 print("No file available. Please rerun the whole process / load data first.")
                 sys.exit(1)
             self.timestamp = latest_file[-20:-4]
-            eval_data_path = f'{self.config["prep"]["DataOutputDir"]}{latest_file}'
+            eval_data_path = latest_file
         else:
             self.timestamp = custom_timestamp
             eval_data_path = f'{self.config["prep"]["DataOutputDir"]}model_eval_data_{self.timestamp}.pkl'
@@ -44,31 +50,30 @@ class Plots:
         self.export_path = f'{self.setup.ROOT_PATH}{self.config["prep"]["ExportDir"]}{self.timestamp}/'
         if not os.path.isdir(self.export_path): os.mkdir(self.export_path)
         
-        self.logger.info(f"Loading latest eval data pickle: {eval_data_path}")
+        # Load model evaluation data
+        self.logger.info(f"Loading latest eval data pickle:\t{eval_data_path}")
         with open(eval_data_path, 'rb') as handle:
             self.eval_data = pickle.load(handle)
+        self.logger.info("Loaded\n")    
+            
         # Load split windows dictionary
         with open(self.config["prep"]["WindowSplitDict"], 'rb') as handle:
             self.window_dict = pickle.load(handle)
-
-        # Load model config dictionary
-        # model_config_dict_path = f'{self.config["prep"]["ModelConfigDir"]}model_config_{self.timestamp}.json'
-        # self.logger.info(f"Loading model config json: {model_config_dict_path}")
-        # with open(model_config_dict_path, 'rb') as handle:
-        #     self.model_config_dict = json.load(handle)
         
         # Load peroformance metrics dictionary
         perf_metr_dict_path = f'{self.config["prep"]["ModelMetricsDir"]}performance_metrics_{self.timestamp}.json'
-        self.logger.info(f"Loading performance metrics json: {perf_metr_dict_path}")
+        self.logger.info(f"Loading performance metrics json:\t{perf_metr_dict_path}")
         with open(perf_metr_dict_path, 'rb') as handle:
             self.perf_metr_dict = json.load(handle)
+        self.logger.info("Loaded\n")
 
         # Load model equity line
         model_eqline_dict_path = f'{self.config["prep"]["DataOutputDir"]}eq_line_{self.timestamp}.pkl'
         # model_eqline_dict_path = f'reports/export/{self.timestamp}/eq_line_{self.timestamp}.pkl'
-        self.logger.info(f"Loading model equity line: {model_eqline_dict_path}")
+        self.logger.info(f"Loading model equity line:\t\t{model_eqline_dict_path}")
         with open(model_eqline_dict_path, 'rb') as handle:
             self.eq_line_array = pickle.load(handle)
+        self.logger.info("Loaded\n")
 
         return 0
 
@@ -82,13 +87,14 @@ class Plots:
 
         plt.figure(figsize=(8, 4))
         self.eval_data["Pred"].plot.hist(bins=bins, color="darkslategrey", ec="darkslategrey")
-        plt.xlabel("Predictions", **self.csfont)
-        plt.ylabel("Frequency", **self.csfont)
-        plt.title("Histogram of model predictions", fontsize=13, **self.csfont)
+        plt.xlabel("Predictions")
+        plt.ylabel("Frequency")
+        plt.title("Histogram of model predictions", fontsize=13)
 
         # Save histogram to .png file
         plt.savefig(f'{self.vis_dir}predictions_histogram_{self.timestamp}.png')
         shutil.copy2(f'{self.vis_dir}predictions_histogram_{self.timestamp}.png', self.export_path)
+        self.logger.info(f"Saved Predictions histogram:\t\t{self.vis_dir}predictions_histogram_{self.timestamp}.png")
 
         if show_results:
             plt.show()
@@ -106,43 +112,159 @@ class Plots:
         :return: 0 if the process was successful
         """
 
+        date_flat_vector = self.window_dict['dates_test'].reshape(-1)
+        
         # Load model parameters and attach them to the plot
+        m = self.perf_metr_dict
+        
         txt = ''''''
-        for k, v in self.perf_metr_dict.items():
-            txt += str(k) + ": " + str(v) + '\n'
-
+        txt += f"{'-'*49}\n"
+        txt += f"| Metric\t| Buy & Hold\t| Strategy\t|\n".expandtabs()
+        txt += f"{'-'*49}\n"
+        extra_tab = "\t" if float(m["EQ_ARC"]) >= 0 else ""
+        txt += f'| aRC\t\t| {m["BH_ARC"]}%\t| {m["EQ_ARC"]}%\t{extra_tab}|\n'.expandtabs()
+        txt += f'| aSD\t\t| {m["BH_ASD"]}\t| {m["EQ_ASD"]}\t|\n'.expandtabs()
+        txt += f'| MLD\t\t| {m["BH_MLD"]}%\t\t| {m["EQ_MLD"]}%\t\t|\n'.expandtabs()
+        txt += f'| IR\t\t| {m["BH_IR"]}\t| {m["EQ_IR"]}\t|\n'.expandtabs()
+        txt += f'| IR**\t\t| {m["BH_IR**"]}\t| {m["EQ_IR**"]}\t|\n'.expandtabs()
+        txt += f'| Positions\t| 1\t\t| {m["POS_CNT"]}\t\t|\n'.expandtabs()
+        txt += f"{'-'*49}"
+        txt_console_print = txt
+        txt = f"{self.timestamp}\n{txt}"
+        
         # Plot the equity line
         plt.figure(figsize=(25, 14))
         plt.text(
-            self.window_dict['dates_test'].reshape(-1)[0], int(np.max(self.eq_line_array) * 0.6),
-            txt, fontsize=12, fontdict=self.csfont
+            date_flat_vector[0], int(np.max([self.eq_line_array, self.eval_data["Real"]]) * 0.75),
+            txt, fontsize=12
         )
-        plt.plot(self.window_dict['dates_test'].reshape(-1), self.eq_line_array.reshape(-1))
-        plt.plot(self.window_dict['dates_test'].reshape(-1), self.window_dict['closes_test'].reshape(-1), color='black')
+        
+        plt.plot(date_flat_vector, self.eq_line_array.reshape(-1))
+        plt.plot(date_flat_vector, self.window_dict['closes_test'].reshape(-1), color='black')
 
         ax = plt.gca()
-        ax.set_title(
-            'Equity Line - Buy&Hold vs LSTM model', va='center', fontsize=15, fontweight='bold', fontdict=self.csfont
-        )
-        ax.xaxis.set_major_locator(mdates.MonthLocator(interval=12))
-        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
-        plt.gcf().autofmt_xdate()
-
-        # Add vertical lines indicating time-steps
-        xticks = ax.get_xticks()
-        for xtick in xticks:
-            ax.vlines(
-                x=xtick,
-                ymin=0,
-                ymax=max(self.window_dict['closes_test'].reshape(-1)),
-                ls='--', alpha=0.3, linewidth=0.5, colors='black'
-            )
+        ax.set_title('Equity Line: Buy&Hold vs LSTM', va='center', fontsize=18)
+        
+        min_year = np.datetime64(date_flat_vector[0], 'Y').astype(int) + 1970
+        max_year = np.datetime64(date_flat_vector[-1], 'Y').astype(int) + 1970
+        
+        # X-ticks and vertical lines
+        ax.xaxis.set_major_locator(mdates.YearLocator(base=1))
+        base = datetime.date(min_year-1, 1, 1)
+        date_list = [base + relativedelta(years=x) for x in range(1, max_year-min_year+3)]
+        for date in date_list:
+            ax.vlines(date, ymin=0, ymax=max(self.window_dict['closes_test'].reshape(-1)),
+                ls='--', alpha=0.3, linewidth=0.5, colors='black')
 
         # Save the plot to .png file
         plt.savefig(f'{self.vis_dir}equity_line_{self.timestamp}.png')
         shutil.copy2(f'{self.vis_dir}equity_line_{self.timestamp}.png', self.export_path)
+        self.logger.info(f"Saved Equity Line plot:\t\t\t{self.vis_dir}equity_line_{self.timestamp}.png")
 
         if show_results:
             plt.show()
+        
+        self.logger.info(f"\nRESULTS:\n{txt_console_print}\n")
+        program_execution_delta = datetime.datetime.now() - self.program_start_time
+        program_execution_time = (datetime.datetime(1970,1,1,0,0,0) + program_execution_delta).strftime("%H:%M")
+        self.logger.info(f"Program ended successfully:\t{time.strftime('%d-%m-%Y %H:%M')}\nExecution time:\t\t\t{program_execution_time}\n{'-'*120}")
+        
+        return 0
+    
+    def multi_equity_line(self, timestamps_to_load):
+        
+        # Load equity line arrays
+        equity_line_arrays_list = []
+        for t in timestamps_to_load:
+            model_eqline_dict_path = f'{self.config["prep"]["DataOutputDir"]}eq_line_{t}.pkl'
+            with open(model_eqline_dict_path, 'rb') as handle:
+                equity_line_arrays_list.append(pickle.load(handle))
+                
+        default_eq_line_shape = equity_line_arrays_list[0].shape
+        for el in equity_line_arrays_list: 
+            if el.shape != default_eq_line_shape:
+                self.logger.error("When ploting multiple equity lines, equity line arrays must be equal by shape.")
+                raise Exception
+        
+        # Load split windows dictionary (regenerate if the latest is not up-to-date)
+        with open(self.config["prep"]["WindowSplitDict"], 'rb') as handle: self.window_dict = pickle.load(handle)
+        date_flat_vector = self.window_dict['dates_test'].reshape(-1)
+        
+        # Load Buy&Hold data from 1st timestamp entry
+        eval_data_path = f'{self.config["prep"]["DataOutputDir"]}model_eval_data_{timestamps_to_load[0]}.pkl'
+        with open(eval_data_path, 'rb') as handle: buy_and_hold_array = pickle.load(handle)["Real"]
+        
+        # Load performance metrics dictionaries
+        performance_metric_dictionaries_list = []
+        for t in timestamps_to_load:
+            perf_metr_dict_path = f'{self.config["prep"]["ModelMetricsDir"]}performance_metrics_{t}.json'
+            with open(perf_metr_dict_path, 'rb') as handle:
+                performance_metric_dictionaries_list.append(json.load(handle))
+        
+        # Create a dictionary with average test results
+        mean_res_dict = {
+            "EQ_ARC": 0.0
+            ,"EQ_ASD": 0.0
+            ,"EQ_MLD": 0.0
+            ,"EQ_IR": 0.0
+            ,"EQ_IR**": 0.0
+            ,"POS_CNT": 0.0
+        }
+        for perf_dict in performance_metric_dictionaries_list:
+            for k in perf_dict.keys():
+                if k in mean_res_dict.keys():
+                    mean_res_dict[k] += float(perf_dict[k])/len(timestamps_to_load)
+        
+        mean_res_dict["EQ_ARC"] = str(format(mean_res_dict["EQ_ARC"], '.2f'))
+        mean_res_dict["EQ_ASD"] = str(format(mean_res_dict["EQ_ASD"], '.4f'))
+        mean_res_dict["EQ_MLD"] = str(format(mean_res_dict["EQ_MLD"], '.2f'))
+        mean_res_dict["EQ_IR"] = str(format(mean_res_dict["EQ_IR"], '.4f'))
+        mean_res_dict["EQ_IR**"] = str(format(mean_res_dict["EQ_IR**"], '.4f'))
+        mean_res_dict["POS_CNT"] = str(format(mean_res_dict["POS_CNT"], '.0f'))
+        
+        txt = ''''''
+        txt += f"{'-'*49}\n"
+        txt += f"| Metric\t| Buy & Hold\t| Strategy mean\t|\n".expandtabs()
+        txt += f"{'-'*49}\n"
+        extra_tab = "\t" if float(mean_res_dict["EQ_ARC"]) >= 0 else ""
+        txt += f'| aRC\t\t| {performance_metric_dictionaries_list[0]["BH_ARC"]}%\t| {mean_res_dict["EQ_ARC"]}%\t{extra_tab}|\n'.expandtabs()
+        txt += f'| aSD\t\t| {performance_metric_dictionaries_list[0]["BH_ASD"]}\t| {mean_res_dict["EQ_ASD"]}\t|\n'.expandtabs()
+        txt += f'| MLD\t\t| {performance_metric_dictionaries_list[0]["BH_MLD"]}%\t\t| {mean_res_dict["EQ_MLD"]}%\t\t|\n'.expandtabs()
+        txt += f'| IR\t\t| {performance_metric_dictionaries_list[0]["BH_IR"]}\t| {mean_res_dict["EQ_IR"]}\t|\n'.expandtabs()
+        txt += f'| IR**\t\t| {performance_metric_dictionaries_list[0]["BH_IR**"]}\t| {mean_res_dict["EQ_IR**"]}\t|\n'.expandtabs()
+        txt += f'| Positions\t| 1\t\t| {mean_res_dict["POS_CNT"]}\t\t|\n'.expandtabs()
+        txt += f"{'-'*49}"
+        
+        # Plot the equity line
+        plt.figure(figsize=(25, 14))
+        plt.text(
+            date_flat_vector[0], int(np.max([equity_line_arrays_list[0], buy_and_hold_array]) * 0.75),
+            txt, fontsize=12
+        )
+        
+        for eq_line_array in equity_line_arrays_list:
+            plt.plot(date_flat_vector, eq_line_array.reshape(-1))
+        plt.plot(date_flat_vector, self.window_dict['closes_test'].reshape(-1), color='black', linewidth=3.0)
 
+        ax = plt.gca()
+        ax.set_title('Equity Line: Buy&Hold vs LSTM', va='center', fontsize=18)
+        
+        min_year = np.datetime64(date_flat_vector[0], 'Y').astype(int) + 1970
+        max_year = np.datetime64(date_flat_vector[-1], 'Y').astype(int) + 1970
+        
+        # X-ticks and vertical lines
+        ax.xaxis.set_major_locator(mdates.YearLocator(base=1))
+        base = datetime.date(min_year-1, 1, 1)
+        date_list = [base + relativedelta(years=x) for x in range(1, max_year-min_year+3)]
+        for date in date_list:
+            ax.vlines(date, ymin=0, ymax=max(self.window_dict['closes_test'].reshape(-1)),
+                ls='--', alpha=0.3, linewidth=0.5, colors='black')
+        
+        # Save the plot to .png file
+        plt.savefig(f'{self.vis_dir}equity_line_summary_{timestamps_to_load[-1]}.png')
+        self.export_path = f'{self.setup.ROOT_PATH}{self.config["prep"]["ExportDir"]}{timestamps_to_load[-1]}/'
+        shutil.copy2(f'{self.vis_dir}equity_line_summary_{timestamps_to_load[-1]}.png', self.export_path)
+        self.logger.info(f"Saved Equity Line summary plot for {len(timestamps_to_load)} test cases:\t{self.vis_dir}equity_line_summary{timestamps_to_load[-1]}.png")
+
+        self.logger.info(f"\nRESULTS:\n{txt}\n")        
         return 0